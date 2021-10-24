968984f8f77cba015b6b92f5a70d8349cb312a7e
Showrin <showrinece15@gmail.com>
2021-10-24 23:36:54 +0600

    Authenticate user using firebase authentication token

--- app/http/middleware/middleware.go
+++ app/http/middleware/middleware.go
@@ -1,20 +1,35 @@
 package middleware
 
 import (
+	"context"
+	"locally/goback/config"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
 func AdminMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authorizedToken := c.Request.Header["Token"]
+		firebaseAuth := config.SetupFirebase()
+		authorizationToken := c.GetHeader("Authorization")
 
-		if authorizedToken == nil {
-			c.AbortWithStatusJSON(401, gin.H{
-				"error": "no authentication token found",
-			})
+ 		idToken := strings.TrimSpace(strings.Replace(authorizationToken, "Bearer", "", 1))
+
+		if idToken == "" {
+  			c.JSON(http.StatusBadRequest, gin.H{"error": "Id token not available"})
+  			c.Abort()
+  			return
+		}
+
+		token, err := firebaseAuth.VerifyIDToken(context.Background(), idToken)
+		  
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+			c.Abort()
 			return
 		}
-		c.Set("token", authorizedToken)
+		c.Set("token", token.UID)
 		c.Next()
 	}
 }
